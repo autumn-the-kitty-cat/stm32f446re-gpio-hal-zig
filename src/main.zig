648f28e47a12ed3217bf502e4701e6e0a99def32
autumn-the-kitty-cat <theodorebaker08@gmail.com>
2025-02-17 01:55:04 -0500

    start of example

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,14 @@
-const gpio = @import("gpio.zig");
+// https://github.com/artemderkach/STM32F446RE-examples-zig/blob/main/021_led_registers/main.zig
 
-pub fn main() !void {}
+const gpio = @import("gpio.zig");
+const rcc = @import("rcc.zig");
+
+pub fn main() !void {
+    // TODO: enable clock access
+
+    // Set PA5 to output
+    gpio.GPIOA.MODER.MODER5 = gpio.MODE.Output;
+
+    // Turn on PA5
+    gpio.GPIOA.ODR.ODR5 = 1;
+}
