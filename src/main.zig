12c759cc940e99a5a10c10edd120a25a8b4cbb0f
autumn-the-kitty-cat <theodorebaker08@gmail.com>
2025-02-17 17:03:01 -0500

    working main.zig

--- src/main.zig
+++ src/main.zig
@@ -3,13 +3,25 @@
 const gpio = @import("gpio.zig");
 const rcc = @import("rcc.zig");
 
-pub fn main() !void {
+pub export fn _start() void {
     // Enable clock access
     rcc.RCC.AHB1ENR.GPIOAEN = 1;
 
     // Set PA5 to output
-    gpio.GPIOA.MODER.MODER5 = gpio.MODE.Output;
+    gpio.GPIOA.MODER.MODER4 = gpio.MODE.Output;
 
-    // Turn on PA5
-    gpio.GPIOA.ODR.ODR5 = 1;
+    // Toggle PA5
+    while (true) {
+        gpio.GPIOA.ODR.ODR4 = ~gpio.GPIOA.ODR.ODR4;
+        delay(500000);
+    }
+}
+
+fn delay(time: u32) void {
+    var i: u32 = 0;
+    while (i < time) {
+        // @import("std").mem.doNotOptimizeAway(i);
+        asm volatile ("nop");
+        i += 1;
+    }
 }
